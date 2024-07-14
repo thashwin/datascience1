fe870f25013363ef4eeb01eb8bc1b64aaca273ab
thashwin <thashwinmonnappa619@gmail.com>
2024-07-14 22:53:42 +0530

    Commit 3 - .py to .csv

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # datascience1
 Exploring datascience projects!
+
+## Contents in datascience1
+### <download_csv.py>
+Run the above file to download the .csv directly from kaggle
+[Make sure that kaggle.json file is in "c:\\Users\\admin\\.kaggle\\"]
+
+Dataset link: https://www.kaggle.com/datasets/shibumohapatra/house-price
+
+#### or run this in cmd to download dataset.zip directly from kaggle
+kaggle datasets download -d shibumohapatra/house-price 
+
+#### dataset-file
+dataset = 'shibumohapatra/house-price'  # 'dataset-owner/dataset-name'
+file_name = '1553768847-housing.csv'
---
+++ download_csv.py
@@ -0,0 +1,26 @@
+import os
+import kaggle
+import pandas as pd
+import zipfile
+
+# Ensure the kaggle.json file is in the ~/.kaggle directory or set the environment variables directly
+os.environ['KAGGLE_CONFIG_DIR'] = os.path.expanduser('~/.kaggle')
+
+# Replace with your Kaggle dataset details
+dataset = 'shibumohapatra/house-price'
+file_name = '1553768847-housing.csv'
+
+# Download the dataset file
+kaggle.api.dataset_download_file(dataset, file_name, path='./')
+
+# If the file is downloaded as a zip file, extract it
+zip_file_path = f'{file_name}.zip'
+if os.path.exists(zip_file_path):
+    with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
+        zip_ref.extractall('.')
+    # Delete the ZIP file after extraction
+    os.remove(zip_file_path)
+
+# Read the CSV file using pandas
+data = pd.read_csv(file_name)
+print(data.head())
